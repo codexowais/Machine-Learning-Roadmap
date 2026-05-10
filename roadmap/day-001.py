7313a8faa3900c3607ed50d31da243d081b38222
owais <owaisop15@gmail.com>
2026-05-10 19:09:43 +0530

    day one completed

--- roadmap/day-001.py
+++ roadmap/day-001.py
@@ -10,3 +10,40 @@
 #
 
 # Code here
+#Student report
+students = {
+
+    "owais":{
+        "maths":90,
+        "english":90,
+        "dsa":100
+    },
+
+    "rishan":{
+        "maths":98,
+        "english":89,
+        "dsa":77
+    },
+
+    "sameer" : {
+        "maths":56,
+        "english":99,
+        "dsa":100
+    }
+
+}
+
+for student_name, data in students.items():
+
+    total_marks = (
+        data["maths"] +
+        data["english"] +
+        data["dsa"]
+    )
+
+    average = total_marks / 3
+
+    print("Student:", student_name)
+    print("Total Marks:", total_marks)
+    print("Average:", average)
+    print()
