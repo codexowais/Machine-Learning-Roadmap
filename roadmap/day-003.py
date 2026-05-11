c860fef77dea2de5ae19dc19c62bab7e49827535
owais <owaisop15@gmail.com>
2026-05-12 01:27:53 +0530

    day 3 done

--- roadmap/day-003.py
+++ roadmap/day-003.py
@@ -10,3 +10,28 @@
 #
 
 # Code here
+names = [
+    "  OWAIS SUHAIL ",
+    "rIShAn khan",
+    " SAMEER  ALI "
+]
+
+emails = [
+    " OWAIS@GMAIL.COM ",
+    "RiShAn123@Yahoo.Com ",
+    " SAMEER@OUTLOOK.COM"
+]
+
+for name in names:
+
+    cleaned_name = name.strip().lower()
+
+    print(cleaned_name)
+
+print()
+
+for email in emails:
+
+    cleaned_email = email.strip().lower()
+
+    print(cleaned_email)
