a9ebad075467fbe2e538b89e7c726689a1408345
owais <owaisop15@gmail.com>
2026-05-23 01:31:37 +0530

    Day 6 done

--- roadmap/day-006.py
+++ roadmap/day-006.py
@@ -10,9 +10,56 @@
 #
 
 # Code here
-name = input("Enter your name")
+
+def calculate_average(marks):
+
+    total = sum(marks.values())
+    average = total / len(marks)
+
+    return average
+
+
+def assign_grade(average):
+
+    if average >= 90:
+        return "A"
+
+    elif average >= 75:
+        return "B"
+
+    elif average >= 50:
+        return "C"
+
+    else:
+        return "Fail"
+
+
+name = input("Enter student name: ")
+
 marks = {}
-for i in range(5):
-    marks.add
+
+for i in range(3):
+
+    subject = input("Enter subject name: ")
+    score = int(input(f"Enter marks for {subject}: "))
+
+    marks[subject] = score
+
+
+average = calculate_average(marks)
+grade = assign_grade(average)
+
+
+
+with open("detail.txt","a") as file:
+
+
+    file.write("Hello Owais\n")
+
+    file.write(f"Student:{name}\n")
+    file.write(f"Average:{average}\n")
+    file.write(f"Grade:{grade}\n")
+    file.write("------------------------\n")
+
 
 
