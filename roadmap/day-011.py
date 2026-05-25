d1aea56539ee282f3ceb34c088c5f37be03fabbc
owais <owaisop15@gmail.com>
2026-05-25 12:47:19 +0530

    day 11 completed

--- roadmap/day-011.py
+++ roadmap/day-011.py
@@ -10,3 +10,27 @@
 #
 
 # Code here
+class Student:
+    def __init__(self,name,m1,m2,m3):
+        self.name = name
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+
+    def Calculate_average(self):
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+        return (m1+m2+m3)/3
+    
+
+name = input("Enter Name:")
+m1 = int(input("Enter Mark1:"))
+m2 = int(input("Enter Mark2:"))
+m3 = int(input("Enter Mark3:"))
+
+details = Student(name,m1,m2,m3)
+average = details.Calculate_average()
+
+print(details.name)
+print("average",average)
