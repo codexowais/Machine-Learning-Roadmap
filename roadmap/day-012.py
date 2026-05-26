e168fe560aace6d2b9d0dab6cb43a3972202d903
owais <owaisop15@gmail.com>
2026-05-26 20:37:15 +0530

    Day 12 Done

--- roadmap/day-012.py
+++ roadmap/day-012.py
@@ -10,3 +10,49 @@
 #
 
 # Code here
+class BankAccount:
+    def __init__(self,balance):
+        self.balance = balance
+
+    balance = 10000
+
+    def show_balance(self):
+        print("Your Balance is:", self.balance)
+
+    def Deposit(self,amount):
+        self.balance += amount
+        print("Deposited :",amount)
+        print("Updated Balance is :",self.balance)
+
+    def Withdraw(self,amount):
+        if amount>self.balance:
+            print("Insufficient funds")
+        else:    
+            self.balance -= amount
+            print("Withdrawn :",amount)
+            print("Updated Balance is :",self.balance)
+
+account = BankAccount(10000)
+
+while True:                
+
+    
+    print ("1.Check Balance")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+
+
+    choice = int(input("Enter the Operation:"))
+
+    if choice ==1:
+        account.show_balance()
+    elif choice ==2 :
+        amount = int(input("Enter the amount to deposit:"))
+        account.Deposit(amount)
+    elif choice == 3:
+        amount = int(input("Enter the amount to withdraw:"))
+        account.Withdraw(amount)
+    else:
+        print("Thank you")
+        break            
--- roadmap/day_10_student_performance_tracker.py
+++ roadmap/day_10_student_performance_tracker.py
@@ -17,3 +17,68 @@
 
 
 # Start coding your project here.
+import csv
+from pathlib import Path
+
+csv_path = Path(__file__).with_name("students_data.csv")
+
+students = []
+
+with open(csv_path, "r", newline="") as file:
+    reader = csv.reader(file)
+
+    next(reader, None)
+
+    for row in reader:
+        student = {
+            "name": row[0],
+            "english": int(row[1]),
+            "math": int(row[2]),
+            "python": int(row[3]),
+            "dsa": int(row[4])
+        }
+
+        students.append(student)
+
+
+def calculate_avg(student):
+
+    return (
+        student["english"] +
+        student["math"] +
+        student["python"] +
+        student["dsa"]
+    ) / 4
+
+
+def class_topper():
+
+    highest_average = 0
+    topper = None
+
+    for student in students:
+
+        average = calculate_avg(student)
+
+        if average > highest_average:
+            highest_average = average
+            topper = student
+
+    return topper
+
+
+top_student = class_topper()
+
+if top_student:
+
+    average = calculate_avg(top_student)
+
+    print("Class Topper:", top_student["name"])
+    print("Math:", top_student["math"])
+    print("English:", top_student["english"])
+    print("DSA:", top_student["dsa"])
+    print("Python:", top_student["python"])
+    print("Average:", average)
+
+else:
+    print("No student records found.")
