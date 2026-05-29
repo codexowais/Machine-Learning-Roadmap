a0220a2ecef2dc1c1f112a68d1ea215e704ccfca
codexowais <2022508058@svyasa-sas.edu.in>
2026-05-29 21:57:55 +0530

    Day 17 done

--- roadmap/day-017.py
+++ roadmap/day-017.py
@@ -10,3 +10,27 @@
 #
 
 # Code here
+import numpy as np
+
+# celcius = np.array([39,45,96,90.80])
+
+# fahrenheit_loop = []
+
+# for temp in celcius:
+#     fahrenheit_loop.append(float((temp*9/5)+32))ok
+
+# print ("using loop:")
+# print(fahrenheit_loop)    
+
+
+
+# import numpy as np
+# celcius = np.array([0,10,20,30,40])
+# fahrenheit_numpy = (celcius*9/5)+32
+# print ("using loop")
+# print(fahrenheit_numpy)
+
+
+arr = np.array([70,66,56,98])
+cost_after_gst = (arr*1.18)
+print("The cost after gst :",cost_after_gst)
