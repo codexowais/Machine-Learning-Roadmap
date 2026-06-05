dbb9b888fa9e8ad2026507698e6900ed62c572bb
codexowais <2022508058@svyasa-sas.edu.in>
2026-06-06 00:37:45 +0530

    Day 26 done

--- roadmap/day-026.py
+++ roadmap/day-026.py
@@ -10,3 +10,38 @@
 #
 
 # Code here
+import pandas as pd
+
+students = {
+    "Name": ["Ali", "Sara", "Omar", "Noor"],
+    "Math": [90, 85, 78, 95],
+    "Science": [88, 92, 80, 90]
+}
+
+df = pd.DataFrame(students)
+
+print(df)
+df["Average"] = (df["Math"]+df["Science"])/2
+
+
+def assign_grade(average):
+    if average>=90:
+        return "A"
+    elif average>=80:
+        return "B"
+    else:
+        return "C"
+    
+df["Grade"] = df["Average"].apply(assign_grade)
+
+df["Status"] = df["Average"].apply(
+    lambda x : "Pass" if x>= 85 else "Needs Improvement"
+)
+
+leaderboard = df.sort_values(
+    by="Average",
+    ascending = False
+)
+
+print ("======STUDENT LEADERBOARD ====")
+print(leaderboard)
