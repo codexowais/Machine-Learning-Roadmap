8e857f4a37164a0e378c61950483dcf789e27887
codexowais <2022508058@svyasa-sas.edu.in>
2026-06-06 01:54:36 +0530

    Day 28 done

--- roadmap/day-028.py
+++ roadmap/day-028.py
@@ -10,3 +10,21 @@
 #
 
 # Code here
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+students = {
+    "Name": ["Rahul", "Aisha", "Arjun", "Fatima",
+             "Priya", "Omar", "Neha", "David"],
+
+    "Marks": [88, 92, 79, 95, 85, 90, 87, 82],
+
+    "Hours_Studied": [6, 8, 5, 9, 7, 8, 6, 5]
+}
+
+df = pd.DataFrame(students)
+
+sns.histplot(df["Marks"])
+
+plt.show()
